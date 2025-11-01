f969f1cdf0b7ef314fb0c514a623d91ca51c8f2a
polame <polame.dev@gmail.com>
2025-11-01 13:56:55 +0300

    Добавил файл тулчейна Сделал приложение блинка

--- src/main.c
+++ src/main.c
@@ -1,10 +1,13 @@
 #include "cubemx.h"
 
-int main(void)
+#if 1
+// Приложение обычного блинка на HAL
+int main()
 {
     MX_Init();
-
     while (1) {
+        HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
+        HAL_Delay(500);
     }
 }
-
+#endif
